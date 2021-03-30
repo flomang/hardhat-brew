9b80d4cfd1bdb0b4038ac2a9c54f228f22e50163
flomang <inbox.733@gmail.com>
2021-03-29 22:50:01 -0600

    Added revert test for wrong state.

--- test/purchase.ts
+++ test/purchase.ts
@@ -21,40 +21,48 @@ describe("Purchase", () => {
 
   describe("deployment", async () => {
     it("should set contract value to 5", async () => {
-      expect(await purchase.value()).to.eq(5);
+      await expect(await purchase.value()).to.eq(5);
     });
   });
 
   describe("confirm", async () => {
     it("should confirm purchase by the buyer", async () => {
-      expect(await purchase.connect(accounts[1]).confirmPurchase({value: 10})).to.emit(purchase, "PurchaseConfirmed").withArgs(accounts[1].getAddress, 5);
-      expect(await purchase.state()).to.eq(1);
+      await expect(await purchase.connect(accounts[1]).confirmPurchase({value: 10})).to.emit(purchase, "PurchaseConfirmed").withArgs(accounts[1].getAddress, 5);
+      await expect(await purchase.state()).to.eq(1);
     });
 
     it("should confirm recieve by the buyer", async () => {
-      expect(await purchase.connect(accounts[1]).confirmPurchase({value: 10}));
-      expect(await purchase.connect(accounts[1]).confirmReceived())
+      await expect(await purchase.connect(accounts[1]).confirmPurchase({value: 10}));
+      await expect(await purchase.connect(accounts[1]).confirmReceived())
         .to.emit(purchase, "ItemReceived").withArgs(accounts[1].getAddress, 5)
         .to.changeEtherBalance(accounts[1], 5);
 
-      expect(await purchase.state()).to.eq(2);
+      await expect(await purchase.state()).to.eq(2);
 
-      expect(await purchase.connect(accounts[0]).refundSeller())
+      await expect(await purchase.connect(accounts[0]).refundSeller())
         .to.emit(purchase, "SellerRefunded")
         .to.changeEtherBalance(accounts[0], 15);
 
-      expect(await purchase.state()).to.eq(3);
+      await expect(await purchase.state()).to.eq(3);
     });
 
     it("should revert if wrong seller", async () => {
-        expect(await purchase.connect(accounts[1]).confirmPurchase({value: 10}));
-        expect(await purchase.connect(accounts[1]).confirmReceived())
+        await expect(await purchase.connect(accounts[1]).confirmPurchase({value: 10}));
+        await expect(await purchase.connect(accounts[1]).confirmReceived())
           .to.emit(purchase, "ItemReceived").withArgs(accounts[1].getAddress, 5)
           .to.changeEtherBalance(accounts[1], 5);
   
-        expect(await purchase.state()).to.eq(2);
+        await expect(await purchase.state()).to.eq(2);
         await expect(purchase.connect(accounts[1]).refundSeller()).to.be.revertedWith("Only seller can call this.");
-        expect(await purchase.state()).to.eq(2);
-      });
+        await expect(await purchase.state()).to.eq(2);
+
+        await expect(purchase.connect(accounts[0]).abort()).to.be.revertedWith("Invalid state.");
+    });
+
+    it("should revert if wrong state", async () => {
+        await expect(await purchase.connect(accounts[1]).confirmPurchase({value: 10}));
+        await expect(purchase.connect(accounts[0]).abort()).to.be.revertedWith("Invalid state.");
+        await expect(await purchase.state()).to.eq(1);
+    });
   });
 });
