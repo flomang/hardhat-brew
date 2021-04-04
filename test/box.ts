19e90f55ae5752b23671c316be8a34ec3e48d92a
flomang <inbox.733@gmail.com>
2021-04-04 12:57:08 -0600

    Added owner access test for box contract.

--- test/box.ts
+++ test/box.ts
@@ -11,11 +11,13 @@ const { waffle } = hardhat;
 
 describe("Box", () => {
     let owner: Signer;
+    let imposter: Signer;
     let boxContract: Contract;
 
     beforeEach(async () => {
         let accounts = waffle.provider.getWallets();
         owner = accounts[0];
+        imposter = accounts[1];
 
         boxContract = await waffle.deployContract(owner, contractJson);
     });
@@ -25,6 +27,23 @@ describe("Box", () => {
             let ownerAddress: string = await owner.getAddress();
             // employer should be able to send more ether to the contract
             await expect(await boxContract.owner()).to.eq(ownerAddress);
+        });
+    });
+
+    describe("owner access", async () => {
+        it("should allow the owner to store values", async () => {
+            //let ownerAddress: string = await owner.getAddress();
+            // employer should be able to send more ether to the contract
+            await expect(await boxContract.store(123)).to.emit(boxContract, "ValueChanged").withArgs(123);
+
+            await expect(await boxContract.retrieve()).to.eq(123);
+        });
+
+        it("should reject imposters", async () => {
+            let con = boxContract.connect(imposter);
+            //let ownerAddress: string = await owner.getAddress();
+            // employer should be able to send more ether to the contract
+            await expect(con.store(123)).to.revertedWith("Ownable: caller is not the owner");
         });
     });
 });
