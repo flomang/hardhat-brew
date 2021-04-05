9e1115f70a2926ef086a78b304a17cdebe4b3314
flomang <inbox.733@gmail.com>
2021-04-05 10:28:16 -0600

    Added box deploy script.

---
+++ scripts/box.ts
@@ -0,0 +1,28 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+  
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const factory = await ethers.getContractFactory("Box");
+  const contract = await factory.deploy();
+
+  console.log("Contract address:", contract.address);
+  console.log("Deploy hash:", contract.deployTransaction.hash);
+
+  // The contract is NOT deployed yet; we must wait until it is mined
+  await contract.deployed();
+}
+  
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
