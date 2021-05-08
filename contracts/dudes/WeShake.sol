0334c4b964b7ef1479319feef4bf2aecfaaab105
flomang <inbox.733@gmail.com>
2021-05-07 22:14:05 -0600

    Users must register with the contract before agreeing.

--- contracts/dudes/WeShake.sol
+++ contracts/dudes/WeShake.sol
@@ -9,16 +9,19 @@ contract WeShake is Initializable, OwnableUpgradeable {
     string public terms;
     address payable[] public users;
 
-    //mapping(address => Person) public userInfo;
+    mapping(address => Person) public registry;
     Person[] public members; 
 
     struct Person {
         string name;
         address addr;
+        bool agreed;
     }
 
     event NewTermsSet(string terms);
     event PersonAgreed(string name, address fromAddress);
+    event PersonDisagreed(string name, address fromAddress);
+    event PersonRegistered(string name, address fromAddress);
 
     // https://forum.openzeppelin.com/t/how-to-use-ownable-with-upgradeable-contract/3336
     function initialize() public initializer {
@@ -38,27 +41,46 @@ contract WeShake is Initializable, OwnableUpgradeable {
         return members;
     }
 
-    // anybody can agree to the terms?
-    function agree(string memory _name) public {
-
-        require(!checkUserExists(msg.sender), "you have already agreed to this contract!");
-        require(keccak256(abi.encodePacked(terms)) != keccak256(abi.encodePacked("undefined")), "contract terms are undefined");
-
+    function register(string memory _name) public {
+        require(!checkUserExists(msg.sender), "you are already registered");
         address sender = msg.sender;
         Person memory newUser = Person({
             name: _name,
-            addr: msg.sender
+            addr: sender,
+            agreed: false 
         });
 
         members.push(newUser);
         users.push(payable(sender));
+        registry[sender] = newUser;
+        emit PersonRegistered(_name, sender);
+    }
 
-        emit PersonAgreed(_name, sender);
+    function agree() public {
+        require(checkUserExists(msg.sender), "not registered");
+        require(keccak256(abi.encodePacked(terms)) != keccak256(abi.encodePacked("undefined")), "contract terms are undefined");
+
+        Person storage user = registry[msg.sender];
+        user.agreed = true;
+        emit PersonAgreed(user.name, msg.sender);
+    }
+
+    function disagree() public {
+        require(checkUserExists(msg.sender), "not registered");
+        Person storage user = registry[msg.sender];
+        user.agreed = false;
+        emit PersonDisagreed(user.name, msg.sender);
     }
 
     // the owner is only allowed to set the contract terms
     function setTerms(string memory _terms) public onlyOwner {
         terms = _terms;
+
+        // setting new terms sets all members agreed to false
+        for (uint256 i = 0; i < members.length; i++) {
+            members[i].agreed = false;
+        }
+
         emit NewTermsSet(_terms);
     }
 }
